1da62202b937d33a54ee77c2a40e1b121d20a37a
Mehzabin Vohra <vohramezi234@gmail.com>
2023-03-21 16:40:43 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React  from 'react'
 import Navbar from './homepage/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Intro from './homepage/Intro'
  import Reason from './homepage/Reason'
  import Doctor from './homepage/Doctor'
  import Footer from './homepage/Footer'
@@ -14,6 +15,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
       <>
       <Router>
       <Navbar/>
+      <Intro />
      <Reason/>
      <Doctor/>
      <Footer />
---
+++ src/homepage/Intro.js
@@ -0,0 +1,32 @@
+import img1 from './intro1.png'
+import img2 from './intro2.png'
+import './intro.css'
+
+const Intro = ()=> {
+    return(
+        <div className="intro">
+            <h1>Saar Healthcare</h1>
+            <p>Pain Free New Beginning.</p>
+
+            <div className="intro-des">
+            <div className="intro-text">
+            <h2>We help you deal with</h2>
+            <p>
+            Osteoarthritis<br/>
+            Frozen shoulder<br/>
+            Sports injury<br/>
+            Knee pain, Ankle pain<br/>
+            Cervical spondylitis<br/>
+          And mamy more...</p>
+          </div>
+          <div className="image">
+            <img alt="img" src={img1}/>
+            <img alt="img" src={img2}/>
+          </div>
+          </div>
+        </div>
+    )
+
+}
+
+export default Intro;
