90ee17414b4d9150d21420965b9229bc175904dc
Vaidehi Gandhi <vaidehi@Vaidehis-MacBook-Pro.local>
2023-04-10 11:56:56 +0530

    10 April 2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React  from 'react'
-<<<<<<< HEAD
 import Navbar from './homepage/Navigation/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Intro from './homepage/About us/Intro'
  import Reason from './homepage/Choose us/Reason'
  import Doctor from './homepage/Team/Doctor'
-=======
-import Navbar from './homepage/Navbar'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Intro from './homepage/Intro'
- import Reason from './homepage/Reason'
- import Doctor from './homepage/Doctor'
->>>>>>> 5861cbd50a151f4643302cdfe65c35f46f904820
- import Footer from './homepage/Footer'
+ import Footer from './homepage/Footer/Footer'
 import Treatments from './homepage/Treatments'
 // const App = () => {
   // return (
